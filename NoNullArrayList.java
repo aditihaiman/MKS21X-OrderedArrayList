355bf0d6856f04ffc64a6a1a34114161a5c2da58
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-11-01 19:13:52 -0400

    updated set and add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -10,12 +10,42 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public NoNullArrayList(int len) {
-    size = 0;
-    length = len;
+    super(len);
   }
 
-  public void set(int index, T element) {
-    if (element)
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      return super.set(index, element);
+    }
   }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      return super.add(element);
+    }
+  }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      super.add(index, element);
+    }
+  }
+
+
+  /*public static void main(String[] args) {
+    NoNullArrayList<String> arr = new NoNullArrayList<String>(1);
+    arr.add("hello");
+    System.out.println(arr.set(0, "hi"));
+    System.out.println(arr);
+  } */
 
 }
